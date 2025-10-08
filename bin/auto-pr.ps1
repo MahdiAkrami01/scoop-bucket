9243fc4a3de05f5ee882080df2a64db654b0bce3
Mahdi Akrami <mahdi.akrami.pro@gmail.com>
2025-10-08 20:34:50 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "MahdiAkrami01/my-custom-scoop-bucket:main"
+    [String]$upstream = "MahdiAkrami01/my-custom-scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
